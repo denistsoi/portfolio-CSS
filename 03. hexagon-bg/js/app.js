7581e9a12f6743f454422edba94d7612773c5ad7
denistsoi <denistsoi@gmail.com>
2013-07-30 19:54:30 +0800

    updated the js app

--- 03. hexagon-bg/js/app.js
+++ 03. hexagon-bg/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	//console.log("Hello");
 	$('section').click(function(){
 		$('.hexagon:nth-child(1),.hexagon:nth-child(3)').toggle('slow');
-		$('.hexagon:nth-child(2)').animate({'backgroundColor':'#ff9f5f'}, 2000);
+		$('.hexagon:nth-child(2)').animate({'opacity':'0.2s'});
 		console.log("argh");
 	});
 });
