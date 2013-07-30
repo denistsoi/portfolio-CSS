b0fec901d22e1aec8e940f44a47d212575a87003
denistsoi <denistsoi@gmail.com>
2013-07-30 20:30:19 +0800

    first final commit

--- 03. hexagon-bg/js/app.js
+++ 03. hexagon-bg/js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	//console.log("Hello");
+	var $hex = $('.hexagon')
 	$('section').click(function(){
-		$('.hexagon').fadeToggle('slow');
-		$('.hexagon').promise().done(function(){
+		$hex.fadeToggle('slow');
+		$hex.promise().done(function(){
 			$(this).fadeToggle(4600);
 		});
 		//$('.hexagon:nth-child(1)').toggle('slow'));
