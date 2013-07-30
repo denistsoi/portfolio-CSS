a408b78de8aed32ddb8cb139a775c72e5d40e72a
denistsoi <denistsoi@gmail.com>
2013-07-30 20:27:12 +0800

    some interactivity

--- 03. hexagon-bg/js/app.js
+++ 03. hexagon-bg/js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	//console.log("Hello");
 	$('section').click(function(){
-		$('.hexagon:nth-child(1),.hexagon:nth-child(3)').toggle('slow');
-		$('.hexagon:nth-child(2)').animate({'opacity':'0.2s'});
+		$('.hexagon').fadeToggle('slow');
+		$('.hexagon').promise().done(function(){
+			$(this).fadeToggle(4600);
+		});
+		//$('.hexagon:nth-child(1)').toggle('slow'));
 		console.log("argh");
 	});
 });
