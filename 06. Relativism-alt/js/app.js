d6e597a566d16e8af60acbb118b843d1a76503dd
denistsoi <denistsoi@gmail.com>
2013-08-04 00:38:56 +0800

    interesting pop-up js

--- 06. Relativism-alt/js/app.js
+++ 06. Relativism-alt/js/app.js
@@ -1,10 +1,15 @@
-$(document).ready(function(){
-$('.circle').on('mouseover', function(){
-	var $this = $(this);
-  $this.css({'background':'#ef22ef'});
-});
-$('.circle').on('mouseleave', function(){
-	var $this = $(this);
-	$this.css({'background':'#000'});
-});
+$().ready(function(){
+	circle=$('.circle')
+	circle.hover(function(){
+		$(this).toggleClass('purple');
+	});
+	circle.click(function(){
+		console.log('clicked');
+		$(this).animate({
+			top: $(this).position().top - 100
+		})
+		circle.animate({
+			left: $(this).position().left
+		})
+	});
 });
