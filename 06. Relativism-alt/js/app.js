9991008563ee806b6656b0e7cde298d9bef25444
denistsoi <denistsoi@gmail.com>
2013-08-05 17:25:04 +0800

    bla!

--- 06. Relativism-alt/js/app.js
+++ 06. Relativism-alt/js/app.js
@@ -1,18 +1,57 @@
+/*
+declare variables
+*/
 
 
-$().ready(function(){
-	var $this=$(this);
-	circle=$('.circle');	
+
+
+/*
+01 document height and width
+*/
+
+
+
+
+
+/*
+02 mouse hover, toggle class, change color
+*/
+
+
+
+
+$(function(){
+	console.log(xy());
+	var circle=$('.circle');	
+	
 	circle.hover(function(){
-		$(this).toggleClass('purple');
+		$this = $(this);	
+		$this.toggleClass('purple');
 	});
+
+	var clickcount = 0
 	circle.click(function(){
+		++clickcount;
+		console.log(clickcount);
 		console.log('clicked');
-		$(this).animate({
-			top: $(this).position().top - 100
-		})
 		circle.animate({
-			left: $(this).position().left
+			top: $this.position().top + 100
+		}).animate({
+			left: (document.width/2)-240
+		})
+		$this.animate({
+			top: $this.position().top - 60
 		})
 	});
+
+	/*function backgroundChange(){
+		console.log('hi');
+		$this.toggleClass('purple');
+	}*/
+
+	function xy(){
+		var a = document.width;
+		var b = document.height;
+		return a+' '+b;
+	}
 });
